f0524eac2f45963122b9d2f794b4b2e83b0fb780
mkofdwu <mkofdwu@gmail.com>
2026-08-27 19:32:53 +0800

    remove ord countdown

--- src/components/MyNavBar.vue
+++ src/components/MyNavBar.vue
@@ -1,30 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import { RouterLink } from 'vue-router'
-
-const timeUntilORD = ref('')
-
-function updateTimeUntilORD() {
-  const now = new Date().getTime()
-  const timeLeft = new Date(2026, 0, 31).getTime() - now
-
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
-  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000)
-
-  const daysFmt = days.toString().padStart(2, '0')
-  const hoursFmt = hours.toString().padStart(2, '0')
-  const minutesFmt = minutes.toString().padStart(2, '0')
-  const secondsFmt = seconds.toString().padStart(2, '0')
-
-  timeUntilORD.value = `${daysFmt}:${hoursFmt}:${minutesFmt}:${secondsFmt} to ORD`
-}
-
-updateTimeUntilORD()
-setInterval(updateTimeUntilORD, 1000)
-</script>
-
 <template>
   <div class="h-16 flex items-center justify-center z-10 px-10 max-sm:h-14 max-sm:px-0">
     <router-link to="/" class="flex-1 font-semibold text-lg max-lg:hidden">Jia Jie</router-link>
@@ -48,7 +21,7 @@ setInterval(updateTimeUntilORD, 1000)
     >
       links
     </router-link>
-    <span class="flex-1 text-right opacity-60 max-md:hidden">{{ timeUntilORD }}</span>
+    <div class="flex-1 max-lg:hidden"></div>
   </div>
   <div class="line top-16 w-[calc(100vw-5rem)] max-sm:top-14 max-sm:w-screen"></div>
 </template>
